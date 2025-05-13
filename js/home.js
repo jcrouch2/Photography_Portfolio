3eea1a81b86697af7dbb7c73ddc344fd91d7e516
jcrouch2 <jcrouch2@hawaii.edu>
2025-05-12 20:58:06 -1000

    Improve homepage
    The swiper carousel images will be added using javascript.

---
+++ js/home.js
@@ -0,0 +1,32 @@
+const count = 8; //images
+const wrapper = document.querySelector('.swiper-wrapper');
+
+for (let i = 1; i <= count; i++) {
+  const slide = document.createElement('div');
+  slide.classList.add('swiper-slide');
+
+  const img = document.createElement('img');
+  img.src = `assets/homepage-carousel/h${i}.jpg`;
+  img.alt = `Landscape photo ${i}`;
+  img.loading = "lazy";
+
+  slide.appendChild(img);
+  wrapper.appendChild(slide);
+}
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  speed: 1000,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
