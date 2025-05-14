f3d1f9b549e386dfcbb5b0ea8a38fc6b7946bf0d
jcrouch2 <jcrouch2@hawaii.edu>
2025-05-13 19:35:47 -1000

    Try to improve slow image loading times
    Masonry layout starts after each image loads (instead of after all the images load)

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,11 @@
 document.querySelectorAll('.gallery-container').forEach((galleryContainer) => {
   const folder = galleryContainer.dataset.folder;
   const base = galleryContainer.dataset.base;
-  const count = parseInt(galleryContainer.dataset.count);
+  const imgCount = parseInt(galleryContainer.dataset.count);
 
   const folderPath = `assets/optimized/${folder}/`;
 
-  for (let i = 1; i <= count; i++) {
+  for (let i = 1; i <= imgCount; i++) {
     const filename = `${base}${i}.jpg`;
 
     const link = document.createElement('a');
@@ -14,27 +14,34 @@ document.querySelectorAll('.gallery-container').forEach((galleryContainer) => {
 
     const img = document.createElement('img');
     img.src = `${folderPath}${filename}`;
-    // img.loading = 'lazy';
+    img.loading = 'lazy';
 
     link.appendChild(img);
     galleryContainer.appendChild(link);
   }
 
-  imagesLoaded(galleryContainer, function () {
-    new Masonry(galleryContainer, {
-      itemSelector: '.gallery-item',
-      columnWidth: '.gallery-item',
-      percentPosition: true,
-      gutter: 25,
-    });
+  const msnry = new Masonry(galleryContainer, {
+    itemSelector: '.gallery-item',
+    columnWidth: '.gallery-item',
+    percentPosition: true,
+    gutter: 25,
+  });
 
-    lightGallery(galleryContainer, {
-      selector: '.gallery-item',
-      download: false,
-      thumbnail: true,
-      zoom: true,
-      autoplay: true,
-      fullScreen: true,
-    });
+  //try to reduce loading time by starting masonry layout after each image loads (instead of after all images load)
+  imagesLoaded(galleryContainer)
+  .on('progress', function (_instance, image) {
+    if (image.isLoaded) {
+      image.img.classList.add('loaded');
+    }
+    msnry.layout(); 
+  });
+
+  lightGallery(galleryContainer, {
+    selector: '.gallery-item',
+    download: false,
+    thumbnail: true,
+    zoom: true,
+    autoplay: true,
+    fullScreen: true,
   });
 });
